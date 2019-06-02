f07657ff7a2d355f07f9c6908bd4d39bdd2d9771
Lukasz Ostrowski <lukasz@ostrowski.com.pl>
2019-06-02 16:36:06 +0200

    Fixes build

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,14 +12,6 @@ module.exports = {
         {
             resolve: `gatsby-source-filesystem`,
             options: {
-                name: `images`,
-                path: `${__dirname}/src/images`,
-            },
-        },
-
-        {
-            resolve: `gatsby-source-filesystem`,
-            options: {
                 name: `pages`,
                 path: `${__dirname}/src/pages`,
                 ignore: ['*.scss'],
