d861d3b6767912f6fecdd008ecfc87a65235cb1e
Lukasz Ostrowski <lukasz@ostrowski.com.pl>
2019-06-02 12:19:48 +0200

    Cleans other icons

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import styles from './header.module.scss';
 import { SocialMedia } from '../social-media/social-media';
 
-import arrow from '../../icons/arrow_in_circle.svg';
+import Arrow from '../../icons/arrow_in_circle.svg';
 
 export const Header = () => {
     const onScroll = () => {
@@ -15,10 +15,7 @@ export const Header = () => {
     return (
         <header className={styles.header}>
             <div className={styles.top}>
-                <img
-                    src="/WB_logo_horizontal.svg"
-                    className={styles.logo}
-                />
+                <img src="/WB_logo_horizontal.svg" className={styles.logo} />
                 <div>
                     <SocialMedia className={styles.sm} />
                 </div>
@@ -30,10 +27,7 @@ export const Header = () => {
                 designu i programowania.
             </p>
             <a role="button" onClick={onScroll}>
-                <img
-                    className={styles.arrow}
-                    src={arrow}
-                />
+                <Arrow className={styles.arrow} />
             </a>
         </header>
     );
